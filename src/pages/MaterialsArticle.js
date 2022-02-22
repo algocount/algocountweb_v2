09b1621306418445861d553b87e810729a32bf6a
Luca Draisci <luca.draisci@gmail.com>
2022-02-22 16:56:07 +0100

    refactor

---
+++ src/pages/MaterialsArticle.js
@@ -0,0 +1,26 @@
+import Markdown from "react-markdown";
+import { Link } from "react-router-dom";
+import Layout from "../components/Layout/Layout";
+
+const MaterialsArticle = (props) => {
+  return (
+    <Layout>
+      <h1 className="intro header">
+        <Link
+          to="/materials"
+          style={{ opacity: "0.7", color: "black", textDecoration: "none" }}
+        >
+          Materials
+        </Link>
+        <span style={{ fontWeight: "600" }}>{` / ${props.title}`}</span>
+      </h1>
+      <img src={props.imgUrl.replace(".", "")} alt={props.title}/>
+      <Markdown children={props.content} />
+      <a href={props.resUrl}>
+        <button>Go to the risorsa!</button>
+      </a>
+    </Layout>
+  );
+};
+
+export default MaterialsArticle;
