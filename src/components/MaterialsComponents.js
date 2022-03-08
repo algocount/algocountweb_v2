2b8d70c4557ddd7571b92b5a8e5efe45fdd7ffff
Luca Draisci <luca.draisci@gmail.com>
2022-03-08 14:10:42 +0100

    fix img path

--- src/components/MaterialsComponents.js
+++ src/components/MaterialsComponents.js
@@ -105,7 +105,7 @@ const StyledLink = styled(Link)`
 export const MaterialCard = (props) => {
   return (
     <Card {...props}>
-      <Img src={props.imgUrl} />
+      <Img src={require(props.imgUrl)} />
       <Name>{props.title}</Name>
       <Description>{props.description}</Description>
       <BtnContainer>
